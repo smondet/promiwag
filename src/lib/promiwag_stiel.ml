be195e2cfb1f6fe33dd5736792f0e54fb4b997ae
Sebastien Mondet <smondet@dmms-lab22.ifi.uio.no>
2010-07-02 19:27:00 +0200

    Promiwag_stiel: Fixes and submodule To_string.

--- src/lib/promiwag_stiel.ml
+++ src/lib/promiwag_stiel.ml
@@ -49,7 +49,7 @@ type buffer_type =
 
 and int_expression =
   | Int_expr_unary of int_unary_operator * int_expression
-  | Int_expr_binary of int_binary_operator * int_expression
+  | Int_expr_binary of int_binary_operator * int_expression * int_expression
   | Int_expr_variable of int_variable
   | Int_expr_buffer_content of integer_type * buffer_expression
   | Int_expr_literal of int64
@@ -59,6 +59,8 @@ and buffer_expression =
   | Buf_expr_offset of buffer_expression * int_expression
 
 and bool_expression =
+  | Bool_expr_true        
+  | Bool_expr_false       
   | Bool_expr_and of bool_expression * bool_expression
   | Bool_expr_or of bool_expression * bool_expression
   | Bool_expr_not of bool_expression
@@ -74,6 +76,107 @@ type statement =
   | Do_assign_int of int_variable * int_expression
   | Do_assign_buffer of buffer_variable * buffer_expression
   | Do_assign_bool of bool_variable * bool_expression
+
+module To_string = struct
+    
+  let spr = Printf.sprintf
+
+  let  integer_type = function
+    | Type_uint8       -> "uint8"      
+    | Type_uint16      -> "uint16"     
+    | Type_uint32      -> "uint32"     
+    | Type_uint64      -> "uint64"     
+    | Type_uint_native -> "uint_native"
+
+  let int_unary_operator = function
+  | Int_unary_minus             -> "+"
+  | Int_unary_plus              -> "-"
+  | Int_unary_big2little_endian -> "big2ltl"
+  | Int_unary_little2big_endian -> "ltl2big"
+
+  let int_binary_operator = function
+    | Int_binop_add     ->   "+" 
+    | Int_binop_sub     ->   "-" 
+    | Int_binop_mul     ->   "*" 
+    | Int_binop_div     ->   "/" 
+    | Int_binop_mod     ->   "mod" 
+    | Int_binop_bin_and ->   "bin_and" 
+    | Int_binop_bin_or  ->   "bin_or" 
+    | Int_binop_bin_xor ->   "bon_xor" 
+    | Int_binop_bin_shl ->   "<<"  
+    | Int_binop_bin_shr ->   ">>" 
+
+  let bool_binary_operator = function
+    | Bool_binop_equals            -> "=="   
+    | Bool_binop_notequals         -> "!="      
+    | Bool_binop_strictly_greater  -> ">"             
+    | Bool_binop_strictly_lower    -> "<"           
+    | Bool_binop_equal_or_greater  -> "<="             
+    | Bool_binop_equal_or_lower    -> ">="           
+
+
+  let int_variable    s = s
+  let bool_variable   s = s
+  let buffer_variable s = s
+
+  let rec buffer_type = function
+    | Type_sized_buffer   i -> spr "buffer[%d]" i
+    | Type_pointer          -> spr "pointer"
+    | Type_sizable_buffer s -> spr "buffer[%s]" (int_expression s)
+
+  and int_expression = function
+    | Int_expr_unary  (op, ex)        ->
+      spr "(%s %s)" (int_unary_operator op) (int_expression ex)
+    | Int_expr_binary (op, ea, eb)    -> 
+      spr "(%s %s %s)" (int_expression ea)
+        (int_binary_operator op) (int_expression eb)
+    | Int_expr_variable  v            -> spr "%s" (int_variable v)
+    | Int_expr_buffer_content (t, ex) ->
+      spr "(%s @ %s)" (integer_type t) (buffer_expression ex)
+    | Int_expr_literal        i64     -> spr "%s" (Int64.to_string i64)
+
+  and buffer_expression = function
+    | Buf_expr_variable v         -> spr "%s" (buffer_variable v)
+    | Buf_expr_offset (bex, iex)  -> 
+      spr "(%s +> %s)" (buffer_expression bex) (int_expression iex)
+
+  and bool_expression = function
+    | Bool_expr_true   -> "True"     
+    | Bool_expr_false  -> "False"     
+    | Bool_expr_and        (a, b)     ->
+      spr "(%s And %s)" (bool_expression a) (bool_expression b)
+    | Bool_expr_or         (a, b)     ->
+      spr "(%s Or %s)" (bool_expression a) (bool_expression b)
+    | Bool_expr_not        ex         -> spr "(Not %s)" (bool_expression ex)
+    | Bool_expr_binary_int (op, a, b) ->
+      spr "(%s %s %s)" (int_expression a) 
+        (bool_binary_operator op) (int_expression b)
+
+  let rec statement ?(indent=0) =
+    let cur_indent = String.make indent ' ' in
+    let cat = String.concat "" in
+    let catmap f l = cat (List.map f l) in
+    let assign a b = spr "%s%s := %s;\n" cur_indent a b in
+    let indent = indent + 2 in
+    function 
+    | Do_nothing                 -> spr "%sNop;\n" cur_indent
+    | Do_block          e        -> 
+      spr "%s{\n%s%s}\n" cur_indent (catmap (statement ~indent) e) cur_indent
+    | Do_if            (e, a, b) ->
+      spr "%sIf [%s] Then\n%s%sElse\n%s\n"
+        cur_indent (bool_expression e) (statement ~indent a) 
+        cur_indent (statement ~indent b)
+    | Do_while_loop    (e, a)    -> 
+      spr "%sWhile [%s] Do\n%s\n" 
+        cur_indent (bool_expression e) (statement ~indent a)
+    | Do_assign_int    (a, b) -> assign (int_variable    a) (int_expression    b)
+    | Do_assign_buffer (a, b) -> assign (buffer_variable a) (buffer_expression b)
+    | Do_assign_bool   (a, b) -> assign (bool_variable   a) (bool_expression   b)
+
+
+end
+
+
 
 module Construct = struct
 
