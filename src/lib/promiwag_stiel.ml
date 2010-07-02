3a92a84728275164e27f9658cd99d5029d4a3301
Sebastien Mondet <smondet@dmms-lab22.ifi.uio.no>
2010-07-02 19:27:27 +0200

    Promiwag_stiel: Completed the Construct submodule.

--- src/lib/promiwag_stiel.ml
+++ src/lib/promiwag_stiel.ml
@@ -187,40 +187,62 @@ module Construct = struct
 
   let block l = Do_block l
 
-  let conditional
-      ?(block_then) ?(statement_then)
-      ?(block_else) ?(statement_else) condition =
-    let actual_block bl st =
-      match bl, st with
-      | None, None -> Do_nothing
-      | Some l, None -> l
-      | None, Some s -> Do_block s
-      | Some s, Some l -> Do_block (s :: l) in
-    Do_if (condition, actual_block block_then statement_then,
-           actual_block block_else statement_else)
+  let conditional ?(statement_then=Do_nothing) ?(statement_else=Do_nothing)
+      condition =
+    Do_if (condition, statement_then, statement_else)
 
   let while_loop c s = Do_while_loop (c, s)
 
+  let assign_int    a b  = Do_assign_int    (a, b)
+  let assign_buffer a b  = Do_assign_buffer (a, b)
+  let assign_bool   a b  = Do_assign_bool   (a, b)
+
+
   let rec int = function
-    | `uint i -> Int_expr_literal (Int64.of_int i) 
-    | `u64  i -> Int_expr_literal i 
-    | `minus m   -> Int_expr_unary (Int_unary_minus, int m)
-    | `plus  m   -> Int_expr_unary (Int_unary_plus , int m)
-    | `big2lit m -> Int_expr_unary (Int_unary_big2little_endian, int m)
-    | `lit2big m -> Int_expr_unary (Int_unary_little2big_endian, int m)
-    | `add     (a, b) -> fail "todo"     
-    | `sub     (a, b) -> fail "todo"     
-    | `mul     (a, b) -> fail "todo"     
-    | `div     (a, b) -> fail "todo"     
-    | `modulo  (a, b) -> fail "todo"     
-    | `band    (a, b) -> fail "todo"     
-    | `bor     (a, b) -> fail "todo"     
-    | `bin_xor (a, b) -> fail "todo"   
-    | `bin_shl (a, b) -> fail "todo"    
-    | `bin_shr (a, b) -> fail "todo"   
-    | `var v -> Int_expr_variable v
-    | `buffer_content (*integer_type * buffer_expression*)
-      -> fail "NOT IMPLEMENTED"
+    | `U i -> Int_expr_literal (Int64.of_int i) 
+    | `U64  i -> Int_expr_literal i 
+    | `Minus m   -> Int_expr_unary (Int_unary_minus, int m)
+    | `Plus  m   -> Int_expr_unary (Int_unary_plus , int m)
+    | `B2L     m -> Int_expr_unary (Int_unary_big2little_endian, int m)
+    | `L2B     m -> Int_expr_unary (Int_unary_little2big_endian, int m)
+    | `Add     (a, b) -> Int_expr_binary (Int_binop_add    , int a, int b)
+    | `Sub     (a, b) -> Int_expr_binary (Int_binop_sub    , int a, int b)
+    | `Mul     (a, b) -> Int_expr_binary (Int_binop_mul    , int a, int b)
+    | `Div     (a, b) -> Int_expr_binary (Int_binop_div    , int a, int b)
+    | `Mod     (a, b) -> Int_expr_binary (Int_binop_mod    , int a, int b)
+    | `Band    (a, b) -> Int_expr_binary (Int_binop_bin_and, int a, int b)
+    | `Bor     (a, b) -> Int_expr_binary (Int_binop_bin_or , int a, int b)
+    | `Bxor (a, b) -> Int_expr_binary (Int_binop_bin_xor, int a, int b)
+    | `Bshl (a, b) -> Int_expr_binary (Int_binop_bin_shl, int a, int b)
+    | `Bshr (a, b) -> Int_expr_binary (Int_binop_bin_shr, int a, int b)
+    | `Var v -> Int_expr_variable v
+    |   `U8_at b -> Int_expr_buffer_content (Type_uint8       ,  b)
+    |  `U16_at b -> Int_expr_buffer_content (Type_uint16      ,  b)
+    |  `U32_at b -> Int_expr_buffer_content (Type_uint32      ,  b)
+    |  `U64_at b -> Int_expr_buffer_content (Type_uint64      ,  b)
+    | `Unat_at b -> Int_expr_buffer_content (Type_uint_native ,  b)
+
+  let buffer_type = function
+    | `Sized   i -> Type_sized_buffer   i
+    | `Pointer   -> Type_pointer         
+    | `Sizable s -> Type_sizable_buffer s
+
+ let rec buffer = function
+   | `Var v -> Buf_expr_variable v
+   | `Offset (b, i) -> Buf_expr_offset (buffer b, int i)
+
+  let rec bool = function
+    | `T | `True  -> Bool_expr_true
+    | `F | `False -> Bool_expr_false
+    | `And (a, b) -> Bool_expr_and (bool a, bool b)
+    | `Or  (a, b) -> Bool_expr_or  (bool a, bool b)
+    | `Not  a     -> Bool_expr_not (bool a)
+    | `Eq (a, b) -> Bool_expr_binary_int (Bool_binop_equals           , int a, int b)
+    | `Neq(a, b) -> Bool_expr_binary_int (Bool_binop_notequals        , int a, int b)
+    | `Gt (a, b) -> Bool_expr_binary_int (Bool_binop_strictly_greater , int a, int b)
+    | `Lt (a, b) -> Bool_expr_binary_int (Bool_binop_strictly_lower   , int a, int b)
+    | `Ge (a, b) -> Bool_expr_binary_int (Bool_binop_equal_or_greater , int a, int b)
+    | `Le (a, b) -> Bool_expr_binary_int (Bool_binop_equal_or_lower   , int a, int b)
 
 
   module Input_language = struct
