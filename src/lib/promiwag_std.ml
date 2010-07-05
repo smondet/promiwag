7288ca3ab036cb427567ae5f4081243ac75610a1
Sebastien Mondet <smondet@dmms-lab22.ifi.uio.no>
2010-07-05 17:37:22 +0200

    Promiwag_std: Implemented the Environment module.
    It is a purely functional data-structure for handling variable
    environments.

--- src/lib/promiwag_std.ml
+++ src/lib/promiwag_std.ml
@@ -9,7 +9,7 @@ module Ls = struct
     include ExtList.List
     include ListLabels
 
-    let find_opt f l =
+    let find_opt ~f l =
         try Some (List.find f l) with Not_found -> None
 
 end
@@ -41,6 +41,56 @@ module Io = struct
     let open_out f = 
         IO.output_channel (Pervasives.open_out f)
 end
+
+
+module Environment = struct
+
+  type ('variable, 'value) environment = 
+    | Base of ('variable * 'value) list
+    | Pushed of ('variable * 'value) list * ('variable, 'value) environment
+    (* which is a "never-empty-list of lists". *)
+
+  type ('variable, 'value) t = ('variable, 'value) environment
+
+  let empty = Base []
+
+  let push t =
+    Pushed ([], t)
+      
+  let pop = function
+    | Pushed (h, t) -> t
+    | Base _ -> failwith "Env.pop: There is no environment to pop"
+      
+  let add t n v =
+    match t with
+    | Base e -> Base ((n, v) :: e)
+    | Pushed (h, t) -> Pushed ((n, v) :: h, t)
+
+  let rec find_opt ?(cmp=(=)) env variable =
+    match env with
+    | Pushed (h, t) ->
+      begin match Ls.find_opt ~f:(fun (n, v) -> cmp n variable) h with
+      | Some (_, e) -> Some e
+      | None -> find_opt ~cmp t variable
+      end
+    | Base l ->
+      Opt.map snd (Ls.find_opt ~f:(fun (n, v) -> cmp n variable) l)
+
+  let rec iter ~f = function
+    | Pushed (h, t) ->
+      Ls.iter ~f h; iter ~f t
+    | Base l -> Ls.iter ~f l
+
+  let rec metamap ~map ~reduce = function
+    | Pushed (h, t) ->
+      (reduce (Ls.map ~f:map h)) :: (metamap ~map ~reduce t)
+    | Base l -> 
+      [reduce (Ls.map ~f:map l)]
+
+
+end
+
+
 
 
 module Unique = struct
