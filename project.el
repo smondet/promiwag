cecaaf4baf95edb310d0e0ae52161da2968ba0ca
Sebastien Mondet <smondet@ifi.uio.no>
2010-09-01 16:56:51 +0200

    tools: Remove ghistory Emacs function
    This function was redundant, and weaker than the `standard' one.

--- project.el
+++ project.el
@@ -3,21 +3,6 @@
 (chgbg 'g)
 (setq frame-title-format "Promiwag – %b")
 
-
-;(setq history-format "%cD%n%s%b")
-(setq history-format "%s%n%b")
-(defun ghistory (number)
-  (interactive "MNumber of commits: ")
-  ;(shell-command
-  (let ((before (point)))
-    (progn 
-      (shell-command-on-region 
-       (point) (point)
-       (format "git log --reverse -n %s --pretty=format:\"%s\""
-               number history-format)
-       t t)
-      (kill-region before (point)))))
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Compilation:
 
